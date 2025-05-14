7ea2bb2f7a49e23f2109dd659636b97006aef028
MuhammadTaha305 <muhammadtaha305@gmail.com>
2025-05-14 13:45:16 +0500

    Delete my-app-client directory

--- my-app-client/src/dashboard/Details.jsx
+++
@@ -1,7 +0,0 @@
-// import React, { useState } from 'react'
-// import { Card } from "flowbite-react";
-// import { Link } from 'react-router-dom';
-
-const Details = () => {}
-
-export default Details
